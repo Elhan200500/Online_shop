54f944df3c323e4ec75efa22f8afc07a4ad28978
Elhan200500 <kazbaevelhan@gmail.com>
2022-11-23 19:53:32 +0600

    serializers

---
+++ shop/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import Product, Category
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        ordering = ('name',)
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        ordering = ('name',)
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+        index_together = (('id', 'slug'), )
